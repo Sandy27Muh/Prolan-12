012a0b27c86eee6d3a06094170a363012fdba951
Sandy27Muh <Sandy27Muh@gmail.com>
2017-02-28 03:52:09 -0800

    Add files via upload

---
+++ Minggu ke-2/Mahasiswa.java
@@ -0,0 +1,9 @@
+class Mahasiswa
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Nama   : Muh Nur Sanjaya \n");
+        System.out.println ("Nim    : 10115271\n");
+        System.out.println ("Alamat : Jl.Siliwangi");
+    }
+}
