605e8c653e0711f0f5e99ebf2b19a26d6609482d
Sandy27Muh <Sandy27Muh@gmail.com>
2017-03-08 05:30:54 -0800

    Add files via upload

---
+++ Minggu ke-3/Tipedata.java
@@ -0,0 +1,58 @@
+package tipedata;
+
+public class Tipedata {
+
+    public static void main(String[] args) {
+        // Tipe data objek
+        String nama = "Belajar java yuks";
+        System.out.println(nama.length());
+        
+        //Tipe data byte
+        byte maxByte = 127;
+        byte minByte = -128;
+        byte defaultByte = 0;
+        System.out.println(defaultByte);
+        
+        //Tipe data short
+        short maxShort = 32767;
+        short minShort = -32768;
+        short defaultShort = 0;
+        System.out.println(defaultShort);
+        
+        //Tipe data int
+        int maxInt = 2147483647;
+        int minInt = -2147483648;
+        int defaultInt = 0;
+        System.out.println(defaultInt);
+        
+        //Tipe data long
+        long maxLong = 9223372036854775807L;
+        long minLong = -9223372036854775808L;
+        long defaultLong = 0;
+        System.out.println(defaultLong);
+       
+        //Tipe data Float
+        float maxFloat = 3.402823479999999999999999999999f;
+        float minFloat = -3.402823479999999999999999999999f;
+        float defaultFloat = 0;
+        System.out.println(maxFloat);
+        System.out.println(minFloat);
+        
+        //Tipe data double
+        double randomDouble = 1.797693134862315707d;
+        double randomDouble1 = -1.797693134862315708d;
+        double defaultDouble = 0.0;
+        System.out. println(defaultDouble);
+        
+        //Tipe data Boolean
+        boolean menikah = false;
+        System.out.println(menikah);
+        
+        //Tipe data char
+        char A = 'A';
+        System.out.println(A);
+        
+        
+    }
+    
+}
