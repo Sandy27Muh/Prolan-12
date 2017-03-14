5620ae566a61bb631cdd1b51c2e4be0a8251b730
Sandy27Muh <Sandy27Muh@gmail.com>
2017-03-14 08:55:40 -0700

    Add files via upload

---
+++ Minggu ke-4/Jam.java
@@ -0,0 +1,96 @@
+class Jam {
+    int hh,mm,dd;
+	int jam,menit,detik;
+	
+      public Jam () {
+      }
+
+      public Jam(int hh,int mm,int dd) {
+          this.hh = hh;
+          this.mm = mm;
+          this.dd = dd;
+      }
+	  
+      public int getHh () {
+          return hh;
+      }
+
+      public void setHh (int hh) {
+          this.hh = hh;
+      }
+
+      public int getMm () {
+          return mm;
+      }
+
+      public void setMm (int mm) {
+          this.mm = mm;
+      }
+
+      public int getDd () {
+          return dd;
+      }
+
+      public void setDd (int dd) {
+          this.dd = dd;
+      }
+	  
+
+      int ConvertJamToMenit (int jam) {
+          int menit;
+          menit = 1 * 60;
+          return menit ;
+      }
+
+      int ConvertJamToDetik (int jam) {
+          int detik;
+          detik = 1 * 3600;
+          return detik ;
+    }
+
+      int ConvertMenitToDetik (int menit) {
+          int detik;
+          detik = 1 * 60;
+          return detik ;
+      }
+	  
+	  int convertJamToDetik (int jam) {
+          int detik;
+          detik = 1 * 3600;
+          return detik ;
+    }
+	  
+	  int convertMenitToDetik (int menit) {
+          int detik;
+          detik = 1 * 60;
+          return detik ;
+      }
+
+      int ConvertDetikToMenit (int detik) {
+          double menit;
+          menit = 1 / 60;
+          return detik ;
+      }
+    }
+	
+class W7
+{
+    public static void main (String[] args)
+	    {
+        Jam waktu = new Jam () ;
+
+        System.out.println (waktu.ConvertJamToDetik(10));
+        System.out.println (waktu.ConvertJamToMenit(10));
+        System.out.println (waktu.ConvertMenitToDetik(10));
+        System.out.println (waktu.ConvertDetikToMenit(10));
+        }
+	 public static void main (String[] args)
+	    {
+        Jam waktu = new Jam () ;
+
+        System.out.println (waktu.ConvertJamToDetik(10));
+        System.out.println (waktu.ConvertJamToMenit(10));
+        System.out.println (waktu.ConvertMenitToDetik(10));
+        System.out.println (waktu.ConvertDetikToMenit(10));
+        }
+}
---
+++ Minggu ke-4/W7.java
@@ -0,0 +1,24 @@
+package w7;
+
+ public class W7 
+ {
+
+     public static void main (String [] args)
+	     {
+         Jam waktu = new Jam () ;
+
+         System.out.println (waktu.convertJamToDetik(10));
+         System.out.println (waktu.convertJamToMenit(10));
+         System.out.println (waktu.convertMenitToDetik(10));
+         System.out.println (waktu.convertDetikToMenit(10));
+     }
+	 public static void main (String [] args)
+	     {
+         Jam waktu = new Jam () ;
+
+         System.out.println (waktu.convertJamToDetik(10));
+         System.out.println (waktu.convertJamToMenit(10));
+         System.out.println (waktu.convertMenitToDetik(10));
+         System.out.println (waktu.convertDetikToMenit(10));
+         }
+ }
