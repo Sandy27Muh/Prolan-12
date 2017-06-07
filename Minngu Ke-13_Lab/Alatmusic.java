48c46703d03519efab0d178fd6089ed484cf432b
Sandy27Muh <Sandy27Muh@gmail.com>
2017-06-07 21:11:58 +0700

    Update Alatmusic.java

--- Minngu Ke-13_Lab/Alatmusic.java
+++ Minngu Ke-13_Lab/Alatmusic.java
@@ -3,13 +3,13 @@ public class Alatmusic {
         System.out.println("Nama Alatmusic         : " +namaAlatmusic);     
     }
     public <T> void JenisAlatmusic (T jnsAlatmusic) {
-        System.out.println("Jenis Alatmusic       : " +jnsAlatmusic);
+        System.out.println("Jenis Alatmusic        : " +jnsAlatmusic);
     }
     public <T> void TahunBuat (T thnBuat) {
-        System.out.println("Tahun Buat        : " +thnBuat);
+        System.out.println("Tahun Buat             : " +thnBuat);
     }
     public <T,N> void Harga (T x , N y) {
-        System.out.println("Harga Alatmusic     : " +x);
+        System.out.println("Harga Alatmusic        : " +x);
         System.out.println("Jumlah Stok Alatmusic  : " +y);
     } 
     public static void main (String[] args) {
